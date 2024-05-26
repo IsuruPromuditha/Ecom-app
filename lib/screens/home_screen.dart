e046774233fa6b4eab36bbdd22a4b24b8ed20b7e
Isuru <isurupromuditha617@gmail.com>
2024-05-26 21:52:14 +0530

    modify body section first part

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ecommerce/constraints.dart';
@@ -50,10 +51,59 @@ class HomeScreen extends StatelessWidget{
             const Padding(
               padding: EdgeInsets.symmetric(vertical: defaultPadding),
               child: SearchForm(),
-            )
+            ),
+            Row(
+              children: List.generate(
+                5, (index) => CategoryCard(
+                icon: "assets/icons/dress.svg",
+                title: "Dress",
+                press: () {},
+               ),
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.press,
+  });
+
+  final String icon, title;
+  final VoidCallback press ;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: press ,
+      style: OutlinedButton.styleFrom(
+        shape:const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+              Radius.circular(defaultBorderRadius)),
+       ),
+      ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: defaultPadding / 4,
+              vertical: defaultPadding / 2),
+          child: Column(
+            children: [
+              SvgPicture.asset(icon),
+              const SizedBox(height: defaultPadding/2),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+            ],
+          ),
+        ),
     );
   }
 }
